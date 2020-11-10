e46c9b81608ecfd09d3d15c7bdbfc58299ef1e7f
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2020-11-10 17:02:34 +0100

    final

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,6 @@
 using namespace std;
 
 int main() {
-	Encryption z("C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text.txt", "C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text2.txt");
-	Decryption e("C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text2.txt", "C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text3.txt");
+	Encryption testEncry("C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text.txt", "C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text2.txt");
+	Decryption testDecry("C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text2.txt", "C:\\Users\\maxim\\Documents\\Aprog\\C-C++\\C++\\testFichierTexte\\text3.txt");
 }
